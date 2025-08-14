69f66432a1eab9beb53010dc17f4543eb19ee194
ChantellDavis <155310121+ChantellDavis@users.noreply.github.com>
2025-08-14 15:55:28 -0400

    improved js function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 let scoreLeft = document.getElementById("score-left")
 let scoreRight = document.getElementById("score-right")
 let startOver = document.getElementById("start-over")
+let homeTitle = document.getElementById("home-title")
 console.log(scoreRight)
 let count = 0
 
 function one() {
    count += 1
     scoreLeft.innerText = count
+    if (count > 10) {
+       alert("you're ahead LETS GO!")
+         } else if (count === 100){
+       alert("You Won!!🥳")
+         }
 }
 
 function four() {
     count += 1
      scoreRight.innerText = count
+     
  }
 
  function two() {
@@ -39,3 +46,6 @@ function four() {
     scoreRight.innerText = count
     scoreLeft.innerText = count
   }
+
+
+  
